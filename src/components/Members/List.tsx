0ecb1d993b6a91657a89f43224df8257b3dbe06a
sevastosm <sevastosmatzouranis@gmail.com>
2020-01-07 01:03:24 +0200

    Adds add edit delete components

--- src/components/Members/List.tsx
+++ src/components/Members/List.tsx
@@ -1,40 +1,82 @@
-import * as React from 'react';
-import Item from "./Item"
-import {fetchMembers} from "../../helpers/apicalls"
+import * as React from "react";
+import Item from "./Item";
+import { fetchMembers } from "../../helpers/apicalls";
+import ItemDetails from "./Itemdetails";
 
-interface IAppProps {
-}
+interface IAppProps {}
 
-const List: React.FC<IAppProps> = (props) => {
-
+const List: React.FC<IAppProps> = props => {
   const [members, setMembers] = React.useState([]);
- // Similar to componentDidMount and componentDidUpdate:
- React.useEffect(() => {
-    console.log("IN")
-    fetchData()
+  const [itemForm, setItemForm] = React.useState(false);
+  const [formStatus, setFormStatus] = React.useState('add');
+  const [selectedMember, setselectedMember] = React.useState({});
+
+  // Similar to componentDidMount and componentDidUpdate:
+  React.useEffect(() => {
+    fetchData();
     // Update the document title using the browser API
-  },[])
+  }, []);
 
-  const fetchData=()=>
-    fetchMembers().then(response=>{
-        setMembers(response.data)
-     })
-  
-  return members?
-  <ul>
-      <li>
-          <span>SurName</span>
-          <span>Name</span>
-          <span>Phone</span>
-          <span>Email</span>
-          <span>Status</span>
-      </li>
-    {members.map((member:any)=>{
-        console.log("member",member)
-          return <Item key={member.MembersID}  data={member}/>
-      })}</ul>
-  :'loading'
+  const fetchData = () =>
+    fetchMembers().then(response => {
+      setMembers(response.data);
+    });
 
+  const handleAdditem = e => {
+    e.preventDefault();
+    setItemForm(true);
+    setFormStatus('add')
+  };
+
+  const handleSelectedItem = (id: string) => {
+    let selectedItem = members.filter(member => {
+      return member.MembersID === id;
+    });
+    console.log("SelectedItem", selectedItem);
+    setFormStatus('edit')
+    setselectedMember(selectedItem[0]);
+    setItemForm(true);
+
+  };
+
+  return (
+    <div>
+      {members ? (
+        <table style={{ width: "100%", textAlign: "center" }}>
+          <thead>
+          <tr>
+            <th>Actions</th>
+            <th>SurName</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Status</th>
+          </tr></thead>
+          <tbody>
+          {members.map((member: any) => {
+            return (
+              <Item
+                key={member.MembersID}
+                editItem={handleSelectedItem}
+                data={member}
+              />
+            );
+          })}
+          <tr>
+            <td>
+              <button onClick={handleAdditem}>+</button>
+            </td>
+          </tr></tbody>
+        </table>
+      ) : (
+        <div>Loading Data..</div>
+      )}
+
+      {itemForm ? (
+     <ItemDetails members={members[0]} member={selectedMember} status={formStatus} />
+      ) : null}
+      </div>
+  );
 };
 
 export default List;
